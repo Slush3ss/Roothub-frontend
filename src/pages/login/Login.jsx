2aabd9a782973ddc5b2b84bb836130636475b697
Slush3ss <brendonbazilio20@gmail.com>
2025-09-17 19:14:47 -0300

    nomes de classes mudados e adição de delay na animação das particulas

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,21 +9,20 @@ function login() {
 
   return (
     <>
-      <div className="bgSingin p-5 rounded-5">
-        <h1 className="titleSingin fw-bold m-0">Roothub</h1>
+      <div className="bgSingIn p-5 rounded-5">
+        <h1 className="titleSingIn fw-bold m-0">Roothub</h1>
         <form className="d-flex flex-column my-4">
           <input
             type="text"
-            className="inputsSingin mb-4 text-light border-0 p-2 rounded-1"
+            className="inputSingIn mb-4 text-light border-0 p-2 rounded-1"
             placeholder="E-mail"
           ></input>
-          <input type="text" className="inputsSingin text-light border-0 p-2 rounded-1" placeholder="Password"></input>
+          <input type="text" className="inputSingIn text-light border-0 p-2 rounded-1" placeholder="Password"></input>
         </form>
-        <button className="btnSingin rounded-1 fs-3 fw-bold btn w-100">Sing in</button>
+        <button className="btnSingIn rounded-1 fs-3 fw-bold btn w-100">Sing in</button>
       </div>
     </>
   );
-  createParticles();
 }
 
 function createParticles() {
@@ -41,6 +40,7 @@ function createParticles() {
       particleDiv.style.left = Math.random() * 99 + "%";
       particleDiv.style.top = "-10%";
       particleDiv.style.animationDuration = 15 + "s";
+      particleDiv.style.animationDelay = Math.random() * 4
 
       particles.appendChild(particleDiv);
 
@@ -49,7 +49,7 @@ function createParticles() {
           particles.removeChild(particleDiv);
         }
       }, 15000);
-    }, i * 1250);
+    }, i * 2250);
   }
 }
 
